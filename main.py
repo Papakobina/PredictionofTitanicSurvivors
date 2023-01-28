1433965f464e17eb89e4a029c750ec8547c99330
Papakobina <aggreyian3@gmail.com>
2023-01-28 00:02:50 -0500

    updated file

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as py
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Notice that the data has already been split into a test and train dataset 
+# This is a class used to visualize the data
+Titanic_dataset = pd.read_csv("titanic_train.csv")
+print(Titanic_dataset.head())
+print('sjsj')
+
+# class visualization:
+#     def __init__(self):
+#         pass
+
